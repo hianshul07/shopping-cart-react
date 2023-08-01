2ee87d56a14edddb333151dcdea05ac397857319
Anshul Kumar <hianshul07@gmail.com>
2023-08-01 17:42:57 +0530

    phase-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import NavBar from './Components/NavBar'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>heheeh</h1>
+    <div className='App'>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' />
+          <Route path='/cart' />
+        </Routes>
+      </Router>
     </div>
   )
 }
